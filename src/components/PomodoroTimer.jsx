c2399669bbd19fcdb416ee14e02eaec615c64d93
shmp-dev <shmp.dev@gmail.com>
2023-05-28 12:25:34 +0900

    [update]ポモドーロ・タイマーを実装

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,46 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
-          Button,
-          // Alert,
-          // AlertIcon,
-          // AlertDescription,
-          Stack
-        } from '@chakra-ui/react';
+  Button,
+  Stack,
+  Text,
+  List,
+  ListItem,
+  HStack,
+  Spacer
+} from '@chakra-ui/react';
 import { Timer } from './Timer';
 
-// ポモドーロタイマーコンポーネント
 export const PomodoroTimer = (props) => {
   const { scheduleList, viewPomodoroTimer, setViewPomodoroTimer } = props;
-  // const [errorMessage, setErrorMessage] = useState(''); // エラーメッセージ
-  // スケジュール作成
-  // useEffect(() => {
-  //   if (!viewPomodoroTimer) return;
-  //   console.log(scheduleList);
-  //   alert(scheduleList);
-  // }, [viewPomodoroTimer]);
+  const scheduleKeys = Object.keys(scheduleList);
+  const [selectedTimerKey, setSelectedTimerKey] = useState(scheduleKeys[0]);
+  
+  useEffect(() => {
+    // scheduleListが更新されたときに実行
+    const newScheduleKeys = Object.keys(scheduleList);
+    setSelectedTimerKey(newScheduleKeys[0]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scheduleList]);
+  
+  const selectNextTimer = () => {
+    const currentIndex = scheduleKeys.indexOf(selectedTimerKey);
+    const nextIndex = currentIndex + 1;
+    setSelectedTimerKey(scheduleKeys[nextIndex]);
+  };
+
+  const selectPreviousTimer = () => {
+    const currentIndex = scheduleKeys.indexOf(selectedTimerKey);
+    const prevIndex = currentIndex - 1;
+    setSelectedTimerKey(scheduleKeys[prevIndex]);
+  };
+
+  const currentIndex = scheduleKeys.indexOf(selectedTimerKey);
 
   return (
     <>
       { viewPomodoroTimer && 
         <Stack>
-          {/* {errorMessage && (
-            <Alert status="error">
-                <AlertIcon />
-                <AlertDescription>{errorMessage}</AlertDescription>
-            </Alert>
-          )} */}
+          <Timer 
+            key={selectedTimerKey}
+            title={scheduleList[selectedTimerKey].task}
+            workDuration={scheduleList[selectedTimerKey].schedule.workDuration}
+            breakDuration={scheduleList[selectedTimerKey].schedule.breakDuration}
+            iterations={scheduleList[selectedTimerKey].schedule.iterations}
+          />
+          <HStack width="100%">
+            {currentIndex > 0 && <Button onClick={selectPreviousTimer}>＜</Button>}
+            <Spacer />
+            {currentIndex < scheduleKeys.length - 1 && <Button onClick={selectNextTimer}>＞</Button>}
+          </HStack>
+          <List>
+            { Object.keys(scheduleList).map(key => (
+              <ListItem key={key} paddingBottom={'3'}>
+                <Text fontSize={'xl'}>＜{key}＞</Text>
+                <Text>タスク名: {scheduleList[key].task}</Text>
+                <Text>時間: {scheduleList[key].schedule.workDuration}</Text>
+                <Text>休憩時間: {scheduleList[key].schedule.breakDuration}</Text>
+                <Text>反復回数: {scheduleList[key].schedule.iterations}</Text>
+                </ListItem>
+            )) }
+          </List>
           <Button onClick={() => setViewPomodoroTimer(false)}>生成画面へ戻る</Button> 
-          {
-            Object.keys(scheduleList).map(key => (
-              <Timer 
-                key={key}
-                title={scheduleList[key].task}
-                workDuration={scheduleList[key].schedule.workDuration}
-                breakDuration={scheduleList[key].schedule.breakDuration}
-                iterations={scheduleList[key].schedule.iterations}
-              />
-            )) 
-          }
         </Stack>
       }
     </>
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,80 +4,113 @@ import {
           Text,
           VStack,
           useToast,
-          Stack
+          Stack,
+          Spacer,
+          HStack,
+          Card,
+          CardBody,
+          CardHeader
         } from '@chakra-ui/react';
 
 // タイマーコンポーネント
 export const Timer = (props) => {
   const { title, workDuration, breakDuration, iterations } = props;
-
-  // タイマーの分と秒、そしてタイマーが動作中かどうかを管理するための状態を定義します。
+  
   const [minutes, setMinutes] = useState(workDuration);
   const [seconds, setSeconds] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-
-  // トースト（一時的な通知）を表示するためのフックを定義します。
+  const [isWorkPeriod, setIsWorkPeriod] = useState(true);
+  const [currentIteration, setCurrentIteration] = useState(1);
+  
   const toast = useToast();
-
-  // useEffectフックを使用して、タイマーが動作中のときに毎秒更新を行うようにします。
+  
   useEffect(() => {
     let interval = null;
-
-    // タイマーが動作中であれば、以下の処理を毎秒行います。
+    
     if (isRunning) {
-        // 1秒ごとに処理を実行
         interval = setInterval(() => {
-            // 秒が1以上であれば、1秒減らします。
             if (seconds > 0) {
-            setSeconds(seconds - 1);
+                setSeconds(seconds - 1);
             }
-
-            // 秒が0になったときの処理です。
+            
             if (seconds === 0) {
-            // 分も0になった場合、タイマーが終了したとみなします。
-            if (minutes === 0) {
-                clearInterval(interval);  // タイマーをクリアします。
-                // トーストを表示してユーザーに通知します。
-                toast({
-                title: "Time's up!",
-                description: "Take a short break.",
-                status: "success",
-                duration: 3000,
-                isClosable: true,
-                });
-                // 短い休憩のタイマーをセットします。
-                setMinutes(breakDuration);
-                setSeconds(0);
-            } else {
-                // 分がまだ残っている場合、分を1減らし、秒を59にリセットします。
-                setMinutes(minutes - 1);
-                setSeconds(59);
-            }
+                if (minutes === 0) {
+                    clearInterval(interval);
+                    toast({
+                      title: "時間です!",
+                      description: isWorkPeriod ? "休憩を取りましょう。" : "仕事に戻りましょう。",
+                      status: "success",
+                      duration: 3000,
+                      isClosable: true,
+                    });
+
+                    // 休憩時間に移行
+                    if (isWorkPeriod) {
+                      setMinutes(breakDuration);
+                      setSeconds(0);
+                      setIsWorkPeriod(false);
+                    } else {
+                      if (currentIteration === iterations) {
+                        // 全て終了
+                        toast({
+                          title: "終了です!",
+                          description: "次のセッションに進みましょう。",
+                          status: "success",
+                          duration: 10000,
+                          isClosable: true,
+                        });
+                        setIsRunning(false);
+                      } else {
+                        setMinutes(workDuration);
+                        setSeconds(0);
+                        setIsWorkPeriod(true);
+                        setCurrentIteration(currentIteration + 1);
+                      }
+                    }
+                } else {
+                    setMinutes(minutes - 1);
+                    setSeconds(59);
+                }
             } 
         }, 1000);
     } else if (!isRunning && minutes !== 0 && seconds !== 0) {
-      // タイマーが停止している場合、タイマーをクリアします。
       clearInterval(interval);
     }
-
-    // コンポーネントがアンマウントされたときにタイマーをクリアするクリーンアップ関数を定義します。
+    
     return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isRunning, minutes, seconds, toast]);  // これらの状態が変化したときにeffectを再実行します。
-
-  // タイマーの動作状態を切り替える関数を定（続き）義します。
+  }, [isRunning, isWorkPeriod, minutes, seconds, toast, workDuration, breakDuration, currentIteration, iterations]);
+  
   const toggleIsRunning = () => {
     setIsRunning(!isRunning);
-  }
+  }  
+
+  const resetTimer = () => {
+    setMinutes(workDuration);
+    setSeconds(0);
+    setIsRunning(false);
+    setIsWorkPeriod(true);
+    setCurrentIteration(1);
+  } 
 
   return (
-    <Stack> 
-      <VStack p={'3'} border={'1px'} borderRadius={'lg'} spacing={10}>
-        <Text fontSize={'2xl'}>{title}</Text>
-        <Text fontSize="6xl">{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</Text>
-        <Text>反復回数：{iterations}</Text>
-        <Button onClick={toggleIsRunning}>{isRunning ? 'Pause' : 'Start'}</Button>
-      </VStack>
-    </Stack>
+    <Card> 
+      <CardBody>
+        <VStack>
+          <CardHeader fontSize={'2xl'}>
+            <Text>{title}</Text>
+          </CardHeader>
+          {!isWorkPeriod && <Text>（休憩時間）</Text>}
+          <Text fontSize="6xl">{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</Text>
+          <Text>反復回数：{currentIteration}/{iterations}</Text>
+          <Spacer />
+          <CardHeader>
+            <HStack>
+              <Button onClick={toggleIsRunning}>{isRunning ? 'Pause' : 'Start'}</Button>
+              <Button onClick={resetTimer}>Reset</Button> 
+            </HStack>
+          </CardHeader>
+        </VStack>
+      </CardBody>
+    </Card>
   );
 }
